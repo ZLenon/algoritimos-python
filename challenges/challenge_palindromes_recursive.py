afdae2ef70d0948f178d767da710d0a2fa82ad07
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-10 21:50:39 -0300

    Feat: Função recursiva que verifica palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(palavra, low_index, high_index):
+    if len(palavra) <= 0 or type(palavra) != str:
+        return False
+
+    if high_index == low_index:
+        # [::-1] faz a leitura de tras para frente
+        palavra_invertida = palavra[::-1]
+        if palavra_invertida == palavra:
+            return True
+        else:
+            return False
+    else:
+        return is_palindrome_recursive(palavra, low_index + 1, high_index)
